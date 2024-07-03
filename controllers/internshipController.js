6c354d647428d1c5b98e0b2fa9eabc56ae4e4a80
boatnoah <noahskim7@gmail.com>
2024-07-03 15:43:05 -0700

    feat - implemented internship detail page

--- controllers/internshipController.js
+++ controllers/internshipController.js
@@ -14,7 +14,20 @@ const internshipList = expressAsyncHandler(async (req, res, next) => {
 });
 
 const internshipDetail = expressAsyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Internship detail: ${req.params.internshipid}`);
+  const internshipData = await Internship.find({
+    _id: req.params.internshipid,
+  }).exec();
+
+  const companyName = await Company.find({
+    _id: internshipData[0].company,
+  }).exec();
+
+  console.log(companyName);
+
+  res.render("internship_detail", {
+    company: companyName[0].name,
+    internship_detail: internshipData,
+  });
 });
 
 export default { internshipList, internshipDetail };
